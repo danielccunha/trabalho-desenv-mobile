fc23f59f1f2404a8cc8f3ac17ae3108c3cc9cab9
Daniel Cunha <danielcunha54@gmail.com>
2020-09-17 21:42:51 -0300

    feat: Finish web

--- packages/web/src/App.js
+++ packages/web/src/App.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import api from "./services/api";
 
 function App() {
-  return <div className="App"></div>;
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    async function fetchUsers() {
+      const { data } = await api.get("/users");
+      setUsers(data);
+    }
+
+    fetchUsers();
+  }, []);
+
+  return (
+    <div className="App">
+      <h4>Listagem de usuários</h4>
+      <table>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user._id}>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default App;
